9d6e7f492e25ee72bf540a8c03f2da80d01d14bc
crypticwoodwhite <crypticwoodwhite@gmail.com>
2022-07-04 15:25:37 -0700

    Add notes

---
+++ poker.js
@@ -0,0 +1,15 @@
+// from email (there are other hands in addition to these of these): royal flush, straight flush, four of a kind, full house, flush, straight, three of a kind, two pair, pair, and high card.
+
+// royal flush:  five cards of sequential value, all of the same suit, with A being highest value
+// straight flush:  five cards of sequential value, all of the same suit
+// four of a kind: four cards of one value and one card of another value
+// full house: contains three cards of one value and two cards of another value
+// flush: five cards all of the same suit, not all of sequential value
+// straight: five cards of sequential value, not all of the same suit
+// three of a kind: three cards of one value and two cards of two other value
+// two pair: two cards of one value, two cards of another value and one card of a third value
+// high card: a hand that does not fall into any other category
+
+// assume 5 cards are always passed in
+// cards are 2-3 character strings with first 1-2 char being the value, the last character being the suit
+// assume all cards passed in are always valid
